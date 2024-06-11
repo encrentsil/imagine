dcf3073104d4e20d04f1c36553377cf0c62a25e3
Esther Crentsil <nyamekye.nesther.esther599@gmail.com>
2024-06-11 04:10:51 -0700

    Array Methods in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -168,5 +168,43 @@ const alphabets = ['A', 'B','C', 'D'];
 alphabets.join(' ');
 alphabets.join('').split('')
 
+// Write a function that will take a user with first name,last name and return full name
+const user = {
+  firstname: 'Joe',
+  lastname: 'Garner'
+}
+function fullname(user){
+  return {
+    ...user,
+      fullname:`${user.firstname} ${user.lastname}`
+  }
+// ... is used for spreading
+}
+fullname(user); {
+  
+}
+
+// Array Map
+const users = [
+              {firstname: 'Linda',lastname: 'Parker'},
+              {firstname: 'Mark', lastname: 'Twaine'},
+              {firstname: 'Oliver',lastname: 'Queen'},
+              {firstname: 'Stephen', lastname: 'Grider'},
+              {firstname: 'Elon', lastname: 'Musk'}
+              ]
+users.map(fullname);
+
+//Map and return squares of numbers 
+function square (number) {
+  return number ** 2;
+}
+const numbers = [0,1,2,3,4,5]
+numbers.map(square);
+
+//Filter and return even numbers
+function even(number) {
+  return number % 2 === 0;
+}
+numbers.filter(even);            
   
 
